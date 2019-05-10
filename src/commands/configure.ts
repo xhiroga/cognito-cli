ff0d24c9e8acd76c2242f0248aef5d732cb3d34f
Hiroaki Ogasawara <hiroga1030@gmail.com>
2019-05-10 21:04:08 +0900

    add configure

--- src/commands/configure.ts
+++ src/commands/configure.ts
@@ -1,25 +1,50 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from '@oclif/command'
+import * as fs from 'fs-extra'
+import * as path from 'path'
+import * as mkdirp from 'mkdirp'
+
+const initialConfig = {
+  "Auth": {
+    "region": "",
+    "userPoolId": "",
+    "userPoolWebClientId": ""
+  }
+}
 
 export default class Configure extends Command {
   static description = 'describe the command here'
 
   static flags = {
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    help: flags.help({ char: 'h' }),
+    // flag with a value (-r, --region=VALUE)
+    region: flags.string({ char: 'r', description: 'region to access' }),
+    // flag with a value (-u, --userpool=VALUE)
+    userpool: flags.string({ char: 'u', description: 'userpool id to access' }),
+    // flag with a value (-c, --client=VALUE)
+    client: flags.string({ char: 'c', description: 'userpool client id to access' }),
   }
 
-  static args = [{name: 'file'}]
+  static args = [{ name: 'file' }]
 
   async run() {
-    const {args, flags} = this.parse(Configure)
+    const { args, flags } = this.parse(Configure)
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from /Users/hiroaki/Dev/pj/jic/repos/cognito-userpool-cli/src/commands/configure.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    mkdirp.sync(this.config.configDir);
+    const configPath = path.join(this.config.configDir, 'config.json')
+
+    let userConfig
+    if (fs.existsSync(configPath)) {
+      userConfig = await fs.readJSON(configPath)
+    } else {
+      userConfig = initialConfig
     }
+
+    if (flags.region) { userConfig.Auth.region = flags.region }
+    if (flags.userpool) { userConfig.Auth.userPoolId = flags.userpool }
+    if (flags.client) { userConfig.Auth.userPoolWebClientId = flags.client }
+
+    this.log(`config.Auth: ${JSON.stringify(userConfig.Auth)}`)
+
+    fs.writeFile(configPath, JSON.stringify(userConfig, null, 2))
   }
 }
